1609d910e36c9ae562c5a79cd3c029385873d817
Pierre Sjöberg <pierre.sjoberg@kau.se>
2024-04-16 11:18:46 +0200

    F2 slutkod

--- F2/Kodexempel 1/script-01.js
+++ F2/Kodexempel 1/script-01.js
@@ -1,52 +1,68 @@
 window.addEventListener('load', loadData);
 
 function loadData() {
+
+  document.querySelector('#search-form').addEventListener('submit', handleSearchformSubmit);
+  document.querySelector('#preloader').classList.add('d-none');
+
+}
+
+function handleSearchformSubmit(kalkon) {
+  kalkon.preventDefault();
+
   let container = document.querySelector('#content');
   let preloader = document.querySelector('#preloader');
 
-  window.fetch('https://restcountries.com/v3.1/name/sweden')
+  preloader.classList.remove('d-none');
+  let txtland = document.querySelector('#search').value;
+  window.fetch('https://restcountries.com/v3.1/name/' + txtland)
     .then(function(response) {
       return response.json()
     })
     .then(function(data) {
       console.log(data);
-      let countryData = data[0];
+      
 
-      let card = document.createElement('div');
-      card.className = 'card';
-      card.style.maxWidth = '20rem';
-      container.appendChild(card);
+      for(let i=0; i< data.length; i++) {
 
-      let cardImage = document.createElement('img');
-      cardImage.className = 'card-img-top';
-      cardImage.src = countryData.flags.png;
-      card.appendChild(cardImage)
+        let countryData = data[i];
 
-      let cardBody = document.createElement('div');
-      cardBody.className = 'card-body';
-      card.appendChild(cardBody);
+        let card = document.createElement('div');
+        card.className = 'card';
+        card.style.maxWidth = '20rem';
+        container.appendChild(card);
 
-      let cardTitle = document.createElement('h5');
-      cardTitle.className = 'card-title';
-      cardTitle.textContent = countryData.name;
-      cardBody.appendChild(cardTitle)
+        let cardImage = document.createElement('img');
+        cardImage.className = 'card-img-top';
+        cardImage.src = countryData.flags.png;
+        card.appendChild(cardImage)
 
-      let capital = document.createElement('p');
-      capital.className = 'card-text';
-      capital.innerHTML = '<b>Capital:</b> ' + countryData.capital;
-      cardBody.appendChild(capital)
+        let cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+        card.appendChild(cardBody);
 
-      let area = document.createElement('p');
-      area.className = 'card-text';
-      area.innerHTML = '<b>Area:</b> ' + countryData.area.toLocaleString('en') + ' km<sup>2</sup>';
-      cardBody.appendChild(area)
+        let cardTitle = document.createElement('h5');
+        cardTitle.className = 'card-title';
+        cardTitle.textContent = countryData.name.official;
+        cardBody.appendChild(cardTitle)
 
-      let population = document.createElement('p');
-      population.className = 'card-text';
-      population.innerHTML = '<b>Population:</b> ' + countryData.population.toLocaleString('en');
-      cardBody.appendChild(population)
+        let capital = document.createElement('p');
+        capital.className = 'card-text';
+        capital.innerHTML = '<b>Capital:</b> ' + countryData.capital;
+        cardBody.appendChild(capital)
 
-      preloader.classList.add('d-none')
+        let area = document.createElement('p');
+        area.className = 'card-text';
+        area.innerHTML = '<b>Area:</b> ' + countryData.area.toLocaleString('en') + ' km<sup>2</sup>';
+        cardBody.appendChild(area);
+
+        let population = document.createElement('p');
+        population.className = 'card-text';
+        population.innerHTML = '<b>Population:</b> ' + countryData.population.toLocaleString('en');
+        cardBody.appendChild(population);
+    }
+      preloader.classList.add('d-none');
 
     });
+
 }
--- F2/Kodexempel 2/script.js
+++ F2/Kodexempel 2/script.js
@@ -12,13 +12,46 @@ window.addEventListener('load', () => {
 
 });
 
-
+/**
+ * Makes a request to https://restcountries.eu/rest/v2/name/{query} and displays a table with the results.
+ * @param {string} query The user’s search query
+ * @param {HTMLElement} container The <tbody> element that the result will be printed to
+ */
 let search = (query, container) => {
   container.innerHTML = '';
   window.fetch('https://restcountries.com/v3.1/name/' + encodeURIComponent(query))
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      console.log(data);
+      data.forEach(item => {
+        createTableRow(item, container);
+      })
     });
 }
 
+let createTableRow = (countryData, container) => {
+  let row = document.createElement('tr');
+  container.appendChild(row);
+
+  let name = document.createElement('td');
+  name.textContent = countryData.name.common;
+  row.appendChild(name);
+
+  let capital = document.createElement('td');
+  capital.textContent = countryData.capital;
+  row.appendChild(capital);
+
+  let area = document.createElement('td');
+  area.className = 'text-right';
+  if(countryData.area != null) {
+    area.innerHTML = countryData.area.toLocaleString('en') + ' km<sup>2</sup>';
+  }
+  row.appendChild(area);
+
+  let population = document.createElement('td');
+  population.className = 'text-right';
+  if(countryData.population != null) {
+    population.innerHTML = countryData.population.toLocaleString('en');
+  }
+  row.appendChild(population);
+}
